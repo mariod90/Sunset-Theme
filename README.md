164b95056c0106b890abab567c5b38354f431f79
Mario Madera <mariodml90@gmail.com>
2020-01-04 18:24:50 -0500

    enero 04 - 20

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Sunset Theme
+
+#Premium WordPress Theme
+url course: https://www.youtube.com/watch?v=ViZLtFIcSfo&list=PLriKzYyLb28kpEnFFi9_vJWPf5-_7d3rX&index=1
---
+++ inc/function-admin.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+@package sunset-theme
+
+===========================
+    ADMIN PAGE
+===========================
+*/
+
+function sunset_add_admin_page()
+{
+    add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'sunset-theme-options', 'sunset_theme_create_page', get_template_directory_uri() . '/img/sunset-icon.png', 110);
+}
+add_action('admin_menu', 'sunset_add_admin_page');
+
+function sunset_theme_create_page()
+{
+    //generation of our admin page
+    echo '<div class=\'wrap\'><h1>Sunset Theme Options</h1></div>';
+}
